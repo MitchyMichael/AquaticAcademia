25d54b60a12f71df9004cfb9db871e95a33b0002
Michael Wijaya Sutrisna <michaelwijayasutrisna2002@gmail.com>
2023-06-14 13:40:16 +0700

    Initial commit

---
+++ MC2_AppDev/MC2_AppDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_AppDevApp.swift
+//  MC2_AppDev
+//
+//  Created by Michael Wijaya Sutrisna on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MC2_AppDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
