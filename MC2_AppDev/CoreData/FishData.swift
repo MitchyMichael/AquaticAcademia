efff5a6d6665a90a24c43d145b83077e4e6a143f
annaxatma <aatmadjaja03@student.ciputra.ac.id>
2023-07-02 21:53:03 +0700

    fix error in fetching seedGlossary from fishData

--- MC2_AppDev/CoreData/FishData.swift
+++ MC2_AppDev/CoreData/FishData.swift
@@ -12,34 +12,36 @@ class FishData: ObservableObject {
     static let shared = FishData()
     let container = NSPersistentContainer(name: "ModelData")
     
-    init(){
+    init() {
         container.loadPersistentStores { desc, error in
             if let error = error {
                 print("Failed to load the data \(error.localizedDescription)")
             }
         }
-        
-        func save(context: NSManagedObjectContext) {
-            do {
-                try context.save()
-                print("Data Saved")
-            } catch {
-                print("We could not save the data...")
-            }
+    }
+    
+    func save(context: NSManagedObjectContext) {
+        do {
+            try context.save()
+            print("Data Saved")
+        } catch {
+            print("We could not save the data...")
         }
+    }
+    
+    func seedGlossary() {
+        let context = container.viewContext
         
-        func seedGlossary() {
-            let context = container.viewContext
-            
-            let neon_tetra = FishGlossary(context:context)
-            neon_tetra.latin_name = "Paracheirodon innesi"
-            neon_tetra.english_name = "Neon Tetra"
-            neon_tetra.colony = "School"
-            neon_tetra.environment = "Medium sized, Large sized"
-            neon_tetra.temperament = "Peacefull"
-            neon_tetra.diet = "Ominivore"
-            neon_tetra.compatibility = "Guppy, Platy, Molly"
-            neon_tetra.incompatibility = "Betta, Tiger Barb, Cichlids"
-        }
+        let neon_tetra = FishGlossary(context: context)
+        neon_tetra.latin_name = "Paracheirodon innesi"
+        neon_tetra.english_name = "Neon Tetra"
+        neon_tetra.colony = "School"
+        neon_tetra.environment = "Medium sized, Large sized"
+        neon_tetra.temperament = "Peaceful"
+        neon_tetra.diet = "Omnivore"
+        neon_tetra.compatibility = "Guppy, Platy, Molly"
+        neon_tetra.incompatibility = "Betta, Tiger Barb, Cichlids"
+        
+        save(context: context) // Save the context after adding the glossary entry
     }
 }
